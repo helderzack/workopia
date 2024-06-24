2b6eb20476f2b527bb5ef9b223d616538087180b
Hélder Zacarias <helderzacarias@outlook.com>
2024-06-24 20:34:52 -0300

    Now there's routing and database acces in the project. I'm doing this commit before reestructuring the project into a more similar structure to Laravel.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
-
 require '../helpers.php';
-require basePath('views/home.view.php');
+require basePath('Router.php');
+require basePath('Database.php');
+
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -1,6 +1,5 @@
 <?= loadPartial('head') ?>
 <?= loadPartial('navbar') ?>
-<?= loadPartial('showcase-search') ?>
 <?= loadPartial('top-banner') ?>
 
 <!-- Job Listings -->
@@ -8,7 +7,6 @@
     <div class="container mx-auto p-4 mt-4">
         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
-
             <?php foreach ($listings as $listing) : ?>
                 <div class="rounded-lg shadow-md bg-white">
                     <div class="p-4">
@@ -25,17 +23,13 @@
                                 <span><?= $listing->tags ?></span>,
                             </li>
                         </ul>
-                        <a href="/listings?id=<?= $listing->id ?>" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
+                        <a href="details.html" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
                             Details
                         </a>
                     </div>
                 </div>
             <?php endforeach; ?>
-
-            <a href="/listings" class="block text-xl text-center">
-                <i class="fa fa-arrow-alt-circle-right"></i>
-                Show All Jobs
-            </a>
+        </div>
 </section>
 
 <?= loadPartial('bottom-banner') ?>
